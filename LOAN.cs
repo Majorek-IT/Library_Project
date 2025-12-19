2da6beee4b89f32fe1d58044f6724744ae078a89
Antonio <antwojt2@gmail.com>
2025-12-20 00:23:22 +0300

    The models

---
+++ LOAN.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class LOAN
+{
+	public LOAN()
+	{
+	}
+}
---
+++ USER.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class USER
+{
+    [Key]
+    public int USER_ID { get; set; }
+
+    [Required]
+    public string FIRST_NAME { get; set; }
+
+    [Required]
+    public string LAST_NAME { get; set; }
+
+    [Required, EmailAddress]
+    public string EMAIL { get; set; }
+
+    public ICollection<Loan> Loans { get; set; }
+    public USER()
+	{
+	}
+}
